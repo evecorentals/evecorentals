1997504ac7ba2ec7aa1517387b7c3825da89ef74
Sameer Khan <sameer_khan@epam.com>
2025-10-10 00:35:51 +0530

    fix: add assetprefix constant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const ASSET_PREFIX =
+    process.env.NODE_ENV === "production" ? "/evecorentals" : "";
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/logo.png"
+          src={`${ASSET_PREFIX}/logo.png`}
           alt="EV ECO Rentals logo"
           width={180}
           height={65}
